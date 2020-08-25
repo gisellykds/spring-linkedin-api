d721ea249dd9cbbdd063a284a6c9fa1dec941699
gisellykds <gisellykathellynsilva@gmail.com>
2020-08-25 16:13:15 -0300

    Adicionando estrutura basica api linkedin spring/maven

---
+++ api-linkedin/src/main/java/com/api/linkedin/ApiLinkedinApplication.java
@@ -0,0 +1,13 @@
+package com.api.linkedin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLinkedinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiLinkedinApplication.class, args);
+    }
+
+}
