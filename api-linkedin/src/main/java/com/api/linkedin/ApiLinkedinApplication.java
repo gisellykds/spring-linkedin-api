eefd5cfc2f9da8b42106790afed008be3ba23ae5
gisellykds <gisellykathellynsilva@gmail.com>
2020-08-27 01:12:39 -0300

    Adicionando comunicacao h2 database, logs e message errors

--- api-linkedin/src/main/java/com/api/linkedin/ApiLinkedinApplication.java
+++ api-linkedin/src/main/java/com/api/linkedin/ApiLinkedinApplication.java
@@ -2,8 +2,10 @@ package com.api.linkedin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class ApiLinkedinApplication {
 
     public static void main(String[] args) {
