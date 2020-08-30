3f1f357e3a120c5b13077a9d46724d2e929f3fd3
gisellykds <gisellykathellynsilva@gmail.com>
2020-08-30 16:34:18 -0300

    Atualizando doc swagger apiinfo

--- src/main/java/com/api/linkedin/config/swagger/SwaggerConfig.java
+++ src/main/java/com/api/linkedin/config/swagger/SwaggerConfig.java
@@ -1,12 +1,18 @@
 package com.api.linkedin.config.swagger;
 
+import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static com.google.common.base.Predicates.not;
+import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
 @EnableSwagger2
@@ -17,7 +23,20 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
+                .paths(Predicates.not(PathSelectors.regex("/error.*")))
+                .build()
+                .apiInfo(metaData());
+    }
+
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("API LinkedIn - Java & Spring ")
+                .description("\"Desenvolvido por - Giselly Silva\"")
+                .version("1.0.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
                 .build();
     }
+
 
 }
